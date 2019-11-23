74f43c127d4b748dfa9633a2c1a65f10154e6545
Dayse Soares <daysedesign@gmail.com>
2019-11-23 11:34:46 -0300

    Configurando o spring com bootstrap e jquery

---
+++ src/main/java/br/com/daysesoares/curso/boot/DemoMvcThymeleafApplication.java
@@ -0,0 +1,13 @@
+package br.com.daysesoares.curso.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoMvcThymeleafApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoMvcThymeleafApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/daysesoares/curso/boot/DemoMvcThymeleafApplicationTests.java
@@ -0,0 +1,16 @@
+package br.com.daysesoares.curso.boot;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DemoMvcThymeleafApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
