3459bcd6ab6bb543c0256c2b31195c471adc24f9
Dayse Soares <daysedesign@gmail.com>
2019-11-23 14:43:00 -0300

    Configuracao do JPA para criacao do banco de dados

---
+++ src/main/java/br/com/daysesoares/curso/boot/domain/enums/UF.java
@@ -0,0 +1,50 @@
+package br.com.daysesoares.curso.boot.domain.enums;
+
+public enum UF {
+	
+	AC("AC", "Acre"), 
+	AL("AL", "Alagoas"),
+	AM("AM", "Amazonas"),
+	BA("BA", "Bahia"),
+	CE("CE", "Ceará"),
+	DF("DF", "Distrito Federal"),
+	ES("ES", "Espírito Santo"),
+	GO("GO", "Goiás"),
+	MA("MA", "Maranhão"),
+	MT("MT", "Mato Grosso"),
+	MS("MS", "Mato Grosso do Sul"),
+	MG("MG", "Minas Gerais"),
+	PA("PA", "Pará"),
+	PB("PB", "Paraíba"),
+	PR("PR", "Paraná"),
+	PE("PE", "Pernambuco"),
+	PI("PI", "Piauí"),
+	RJ("RJ", "Rio de Janeiro"),
+	RN("RN", "Rio Grande do Norte"),
+	RS("RS", "Rio Grande do Sul"),
+	RO("RO", "Rondônia"),
+	RR("RR", "Roraima"),
+	SC("SC", "Santa Catarina"),
+	SP("SP", "São Paulo"),
+	SE("SE", "Sergipe"),
+	TO("TO", "Tocantins");
+	
+	private String sigle;
+	private String descricao;
+	
+	private UF(String sigle, String descricao) {
+		this.sigle = sigle;
+		this.descricao = descricao;
+	}
+	public String getSigle() {
+		return sigle;
+	}
+	
+	public String getDescricao() {
+		return descricao;
+	}
+	
+	
+	
+
+}
