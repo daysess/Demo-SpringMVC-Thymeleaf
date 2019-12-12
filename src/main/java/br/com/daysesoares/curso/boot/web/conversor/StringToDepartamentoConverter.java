dda7e2d2f9bf01cc98f0f5bada7b4adc0b185a16
Dayse Soares <daysedesign@gmail.com>
2019-12-11 23:05:13 -0300

    Concluído o crud de cargo

---
+++ src/main/java/br/com/daysesoares/curso/boot/web/conversor/StringToDepartamentoConverter.java
@@ -0,0 +1,25 @@
+package br.com.daysesoares.curso.boot.web.conversor;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.stereotype.Component;
+
+import br.com.daysesoares.curso.boot.domain.Departamento;
+import br.com.daysesoares.curso.boot.service.DepartamentoService;
+
+@Component
+public class StringToDepartamentoConverter implements Converter<String, Departamento>{
+
+	@Autowired
+	private DepartamentoService service;
+	
+	@Override
+	public Departamento convert(String text) {
+		if(text.isEmpty()) {
+			return null;
+		}
+		Long id = Long.valueOf(text);
+		return service.buscarPorId(id);
+	}
+
+}
